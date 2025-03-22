2c741f8063c878df20b4b91bbdde90b2e910f3eb
Eduardo Colon <colon3763@gmail.com>
2025-03-21 20:24:38 -0400

    hiding credentials

---
+++ data/ApplicationDBContextFact.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DotNetEnv;
+
+namespace api_pelicualas.Data
+{
+    public class ApplicationDBContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    {
+        public ApplicationDBContext CreateDbContext(string[] args)
+        {
+            // Cargar las variables de entorno del archivo .env
+            Env.Load();
+
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDBContext(optionsBuilder.Options);
+        }
+    }
+}
