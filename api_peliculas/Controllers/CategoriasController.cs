87affe3a5f011401cff137c3f2da90cfb400eedd
Eduardo Colon <colon3763@gmail.com>
2025-04-08 15:45:25 -0400

    PATCH controller

--- api_peliculas/Controllers/CategoriasController.cs
+++ api_peliculas/Controllers/CategoriasController.cs
@@ -32,7 +32,7 @@ namespace api_peliculas.Controllers
             return Ok(categoriasDTO);
         }
 
-        // GET: api/Categorias/5
+        // GET: api/Categorias/{Id}
         [HttpGet("{Id:int}", Name = "GetCategoria")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -70,7 +70,35 @@ namespace api_peliculas.Controllers
                 ModelState.AddModelError("", $"Algo salió mal al guardar el registro {categoria.Nombre}");
                 return StatusCode(500, ModelState);
             }
-            return Ok(categoria);
+            return CreatedAtRoute("GetCategoria", new { Id = categoria.Id }, categoria);
         }
+
+        // PATCH: api/Categorias/{Id}
+        [HttpPatch("{Id:int}", Name = "ActualizarCategoria")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult ActualizarCategoria(int Id, [FromBody] CategoriaDto categoriaDto)
+        {
+            if (categoriaDto == null || Id != categoriaDto.Id || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var categoria = _mapper.Map<Categoría>(categoriaDto);
+            if (_repo.ExisteCategoria(categoria.Nombre))
+            {
+                ModelState.AddModelError("Nombre", "La categoria ya existe!");
+                return BadRequest(ModelState);
+            }
+            if (!_repo.ActualizarCategoria(categoria))
+            {
+                ModelState.AddModelError("", $"Algo salió mal al actualizar el registro {categoria.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+            categoria.FechaCreación = DateTime.Now;
+            return NoContent();
+        }
+        
     }
 }
